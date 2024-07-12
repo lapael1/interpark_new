a65dcc06d17957a6fab04c4b90dfb966a16fd9ee
lapael1 <lapael6@gmail.com>
2024-07-12 18:10:14 +0900

    인터파크 반응형

--- js/main.js
+++ js/main.js
@@ -1,32 +1,32 @@
-window.onload = function(){
-    // 상단의 마이페이지리스트 기능
-  const mypageBt = document.querySelector(".mypage-bt")  
-  const mypageList = document.querySelector(".mypage-list")
-  mypageBt.addEventListener("click",function(){
-    mypageList.classList.toggle("showMypage")
-  })
+window.onload = function () {
+  // 상단의 마이페이지리스트 기능
+  const mypageBt = document.querySelector(".mypage-bt");
+  const mypageList = document.querySelector(".mypage-list");
+  mypageBt.addEventListener("click", function () {
+    mypageList.classList.toggle("showMypage");
+  });
   // swiper ================
   // 프로모션 스와퍼기능
-  const swPromotion = new Swiper(".sw-promotion" , {
-    slidesPerView : 1,
-    spaceBetween : 24,
+  const swPromotion = new Swiper(".sw-promotion", {
+    slidesPerView: 1,
+    spaceBetween: 24,
     breakpoints: {
-        480 : {
-            slidesPerView : 2
-        },
-        760 : {
-            slidesPerView : 2
-        }
+      480: {
+        slidesPerView: 2,
+      },
+      760: {
+        slidesPerView: 2,
+      },
     },
     // nextEl/prevEl 안에 "".~~ .~~"" 으로 입력해주어야함
-    navigation:{
-        nextEl:".promotion .sw-next",
-        prevEl:".promotion .sw-prev",
+    navigation: {
+      nextEl: ".promotion .sw-next",
+      prevEl: ".promotion .sw-prev",
     },
-    loop : true,
+    loop: true,
     autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-      },
-  })
-}
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+  });
+};
