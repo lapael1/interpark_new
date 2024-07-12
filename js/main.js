b3ab2030b0f2c6012c670c76f5fe3f4bc4dc642b
lapael1 <lapael6@gmail.com>
2024-07-12 14:14:04 +0900

    인터파크 스와이퍼 적용

--- js/main.js
+++ js/main.js
@@ -1,8 +1,32 @@
 window.onload = function(){
     // 상단의 마이페이지리스트 기능
-    const iconArrow = document.getElementById("arrow")
-    const mp_list = document.getElementById("mp_list") 
-    iconArrow.onclick = function(){
-        mp_list.classList.toggle("showmp_list")
-    }
+  const mypageBt = document.querySelector(".mypage-bt")  
+  const mypageList = document.querySelector(".mypage-list")
+  mypageBt.addEventListener("click",function(){
+    mypageList.classList.toggle("showMypage")
+  })
+  // swiper ================
+  // 프로모션 스와퍼기능
+  const swPromotion = new Swiper(".sw-promotion" , {
+    slidesPerView : 1,
+    spaceBetween : 24,
+    breakpoints: {
+        480 : {
+            slidesPerView : 2
+        },
+        760 : {
+            slidesPerView : 2
+        }
+    },
+    // nextEl/prevEl 안에 "".~~ .~~"" 으로 입력해주어야함
+    navigation:{
+        nextEl:".promotion .sw-next",
+        prevEl:".promotion .sw-prev",
+    },
+    loop : true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+  })
 }
